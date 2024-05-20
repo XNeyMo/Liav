42b566e2f0f22b926c7c63528f9692996a847162
XNeyMo <xneymodev@gmail.com>
2024-05-20 10:57:28 -0500

    a

--- backend/app.py
+++ backend/app.py
@@ -8,9 +8,14 @@ from microservices.product import router as product_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://127.17.32.138:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", 'http://172.17.31.138:3000'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
