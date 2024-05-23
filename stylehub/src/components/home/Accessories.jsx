536ea2925964951f7caf89eda9fd574ae181a4cb
Neyan Montes <113221957+XNeyMo@users.noreply.github.com>
2024-05-22 23:23:28 -0500

    Update Accessories.jsx

--- stylehub/src/components/home/Accessories.jsx
+++ stylehub/src/components/home/Accessories.jsx
@@ -1,9 +1,59 @@
-import { React } from 'react';
+import { React, useState } from 'react';
+
+import ProductModal from '../modals/ProductModal';
+
+import useFetchProducts from '../../hooks/useFetchProducts.js';
 
 const Accessories = () => {
+	const products = useFetchProducts('Accessories');
+	const [selectedProduct, setSelectedProduct] = useState(null);
+
+	const handleProductClick = (product) => {
+		setSelectedProduct(product);
+	}
+
+	const handleCloseModal = () => {
+		setSelectedProduct(null);
+	}
+
+	const handleAddToCart = (product) => {
+		handleCloseModal();
+	}
+
 	return (
-		<section>
-			<h1>Accessories</h1>
+		<section className='px-10'>
+			<h1 className='text-5xl font-bold py-5 text-center'>ACCESSORIES</h1>
+
+			<div className='flex gap-4'>
+			        {products.length > 0 ? (
+					products.map(product => (
+						<div className='cursor-pointer' key={product.id} onClick={() => handleProductClick(product)}>
+							<div className='w-[12rem]'>
+								<img src={product.imgref[0]} alt={product.name} />
+							</div>
+
+							<div className='max-w-[12rem] pt-2 text-center text-xs font-bold'>
+								<h2>{product.name}</h2>
+
+								<div className='flex justify-between'>
+									<p>$ {product.price}</p>
+									<p>{product.stock} stock</p>
+								</div>
+							</div>
+						</div>
+					))
+				) : (
+					<p>No products found in the "Accessories" category.</p>
+				)}
+			</div>
+
+			{selectedProduct && (
+				<ProductModal
+					product={selectedProduct}
+					onClose={handleCloseModal}
+					onAddToCart={handleAddToCart}
+				/>
+			)}
 		</section>
 	)
 }
