5d34d24ac3b886d254d09a05060620648ec7bdfb
Neyan Montes <113221957+XNeyMo@users.noreply.github.com>
2024-05-22 23:24:09 -0500

    Update Footwears.jsx

--- stylehub/src/components/home/Footwears.jsx
+++ stylehub/src/components/home/Footwears.jsx
@@ -22,7 +22,7 @@ const Footwears = () => {
 
 	return (
 		<section className='px-10'>
-			<h1 className='text-5xl font-bold py-5 text-center'>TOPS</h1>
+			<h1 className='text-5xl font-bold py-5 text-center'>FOOTWEARS</h1>
 
 			<div className='flex gap-4'>
 			        {products.length > 0 ? (
